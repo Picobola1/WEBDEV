954dca2e884dfd0ed5ed385105e9c3d39ffabe75
Picobola1 <bonilla.ellie@gmail.com>
2026-05-12 16:30:54 -0700

    made everything cleaner

---
+++ index.js
@@ -0,0 +1,9 @@
+
+document.getElementById('junkB').onmouseover = function() {
+  document.getElementById('junk').style.filter = 'drop-shadow(0 0 10px white)';
+};
+
+document.getElementById('junkB').onmouseout = function() {
+  document.getElementById('junk').style.filter = 'none';
+};
+    
